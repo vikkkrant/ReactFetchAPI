ddb6ed768607cf5b1c6fd45267332e29fc68ed1b
vikkkrant <vikrantbiniwale@gmail.com>
2023-03-02 19:51:48 +0530

    dynamic table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
+import UserData from './components/UserData';
 
+
+const API = "https://jsonplaceholder.typicode.com/users"
 function App() {
+
+  const [users,setUsers] = useState([]);
+
+  const fetchUsers= async(url)=>{
+    try{
+      const res = await fetch(url);
+      const data = await res.json();
+      if (data.length > 0){
+        setUsers(data);
+      }
+    }catch(e){
+      console.log(e)
+    }
+
+  }
+
+
+  useEffect(()=>{
+    fetchUsers(API);
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          <UserData users={users}/>
+        </tbody>
+      </table>
     </div>
   );
 }
